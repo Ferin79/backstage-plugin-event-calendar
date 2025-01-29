d4a0119fbd8a67c903bb6bcd58cdff33427dd5ec
ferin79 <ferinpatel79@gmail.com>
2025-01-29 13:46:09 -0400

    fix yarn.lock

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -52,5 +52,5 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
-backend.add(import('@internal/backstage-plugin-event-calendar-backend-backend'));
+backend.add(import('@internal/backstage-plugin-event-calendar-backend'));
 backend.start();
