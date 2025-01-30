a0fd0c664bb25e370cef82dbeb96b9dd30c0cc5e
ferin79 <ferinpatel79@gmail.com>
2025-01-30 18:12:24 -0400

    fix recurring meeting issue

--- plugins/event-calendar-backend/src/utils/getRecurringEvents.ts
+++ plugins/event-calendar-backend/src/utils/getRecurringEvents.ts
@@ -7,30 +7,20 @@ export const getRecurringEvents = (event: Event): Event[] => {
   }
 
   const rule = RRule.fromString(event.rrule);
-  const occurrences = rule.all();
+  const startTime = event.start.split('T')[1];
+  const endTime = event.end.split('T')[1];
 
-  return occurrences.map(startDate => {
-    const start = new Date(startDate);
-    const end = new Date(start);
-    const [startHours, startMinutes] = event.start
-      .split(' ')[1]
-      .split(':')
-      .map(Number);
-    const [endHours, endMinutes] = event.end
-      .split(' ')[1]
-      .split(':')
-      .map(Number);
+  return rule.all().map(date => {
+    const dateString = date.toISOString().split('T')[0];
+    const start = new Date(`${dateString}T${startTime}`);
+    const end = new Date(`${dateString}T${endTime}`);
 
-    start.setHours(startHours, startMinutes, 0, 0);
-    end.setHours(endHours, endMinutes, 0, 0);
+    delete event.rrule;
 
-    const e: Event = {
-      id: event.id,
-      title: event.title,
-      start: start.toUTCString(),
-      end: end.toUTCString(),
-    };
-
-    return e;
+    return {
+      ...event,
+      start: start.toISOString(),
+      end: end.toISOString(),
+    } as Event;
   });
 };
